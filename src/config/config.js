f2dc2407c96e2e96c9d355b52d85f019f00fac04
Daro <daro.wla@hotmail.es>
2020-05-30 17:59:14 -0500

    modify env port

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
 process.env.PORT = process.env.PORT || 3000;
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ mongoose.connect(process.env.URLDB, {
     }
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log(colors.green('******************************'));
     console.log(colors.bold.green(`Server listening on port: ${process.env.PORT}`));
     console.log(colors.green('******************************'));
