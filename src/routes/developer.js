aefcd2a133097f498575960db74586d0eb23b06c
Daro <daro.wla@hotmail.es>
2020-06-03 17:57:39 -0500

    added try catch in post

--- src/routes/developer.js
+++ src/routes/developer.js
@@ -3,6 +3,7 @@ const _ = require('underscore');
 
 const utils = require('../utils/utils');
 const Developer = require('../models/developer_model');
+const { json } = require('body-parser');
 
 const app = express();
 
@@ -25,38 +26,48 @@ app.get('/developer', (req, res) => {
 app.post('/developer', (req, res) => {
     let body = req.body;
     if (body.tecnologias_conocidas.length > 0) {
-        let knowTechnologies = JSON.parse(body.tecnologias_conocidas);
 
-        if (!utils.isIterable(knowTechnologies)) {
+        try {
+            let knowTechnologies = JSON.parse(body.tecnologias_conocidas);
+
+            if (!utils.isIterable(knowTechnologies)) {
+                return res.status(400).json({
+                    Ok: false,
+                    message: 'Technologies are not iterable. You need to send in array format.',
+                });
+            }
+
+            let technologiesToSave = new Array();
+            for (let technology of knowTechnologies) {
+                technologiesToSave.push(technology);
+            }
+
+            let developer = new Developer({
+                nombres_completos: body.nombres_completos,
+                link_github: body.link_github,
+                tecnologias_conocidas: technologiesToSave
+            });
+
+            developer.save((err, developerDB) => {
+                if (err) {
+                    return res.status(400).json({
+                        Ok: false,
+                        message: err,
+                    });
+                }
+                res.json({
+                    Ok: true,
+                    developer: developerDB,
+                })
+            });
+
+        } catch (err) {
             return res.status(400).json({
                 Ok: false,
                 message: 'Technologies are not iterable. You need to send in array format.',
             });
         }
 
-        let technologiesToSave = new Array();
-        for (let technology of knowTechnologies) {
-            technologiesToSave.push(technology);
-        }
-
-        let developer = new Developer({
-            nombres_completos: body.nombres_completos,
-            link_github: body.link_github,
-            tecnologias_conocidas: technologiesToSave
-        });
-
-        developer.save((err, developerDB) => {
-            if (err) {
-                return res.status(400).json({
-                    Ok: false,
-                    message: err,
-                });
-            }
-            res.json({
-                Ok: true,
-                developer: developerDB,
-            })
-        });
     } else {
         return res.status(400).json({
             Ok: false,
